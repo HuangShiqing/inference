19e33ce6a078a2b70912cab14c9edb49fa3d899b
HuangShiqing <hsqyc@163.com>
2021-01-09 23:40:24 +0800

    增加了mnn ultraface模型的支持

--- Makefile
+++ Makefile
@@ -4,19 +4,24 @@ NEON = 1#指定使用NEON
 OPENMP = 1#指定使用openmp
 
 CC = $(CROSS_COMPILE)gcc#指定编译器
-CFLAGS = -I./src/ -I./src/darknet/ -I./src/gstreamer -I./src/gtk/ -I./src/dip/ -I./src/neon/ -Wall#指定头文件目录
+CXX = $(CROSS_COMPILE)g++#指定编译器
+CFLAGS = -I./src/ -I./src/darknet/ -I./thirdparty/ -I./src/gstreamer -I./src/gtk/ -I./src/dip/ -I./src/neon/ -Wall#指定头文件目录
+CXXFLAGS = -std=c++11
+CXXFLAGS += ${CFLAGS}
 CFLAGS += `pkg-config --cflags --libs gstreamer-1.0 gstreamer-video-1.0 gstreamer-app-1.0 gtk+-3.0`
-LDFLAGS = #指定库文件目录
-LIBS = -lm -lpthread#指定库文件名称
+LDFLAGS = -L./resource#指定库文件目录
+LIBS = -lm -lpthread -lMNN#指定库文件名称
 TARGET = inference#最终生成的可执行文件名
 
-VPATH = ./src/:./src/darknet/:./src/gtk/:./src/gstreamer/:./src/dip/:./src/neon/#告诉makefile去哪里找依赖文件和目标文件
+VPATH = ./src/:./src/darknet/:./src/gtk/:./src/gstreamer/:./src/dip/:./src/neon/:./thirdparty/#告诉makefile去哪里找依赖文件和目标文件
 
 #选择debug还是release
 ifeq ($(DEBUG), 1)
 CFLAGS+=-O0 -g
+CXXFLAGS+=-O0 -g
 else
 CFLAGS+=-O1#TODO:使用Ofast会导致gemm_neon.s出现segmentation failt
+CXXFLAGS+=-O1
 endif
 #选择是否使用NEON
 ifeq ($(NEON), 1)
@@ -30,7 +35,7 @@ endif
 OBJ = main.o network.o utils.o im2col.o my_layer.o image.o model.o parser.o \
 	  connected_layer.o convolutional_layer.o maxpool_layer.o avgpool_layer.o \
 	  batchnorm_layer.o activations.o box.o route_layer.o upsample_layer.o yolo_layer.o \
-	  blas.o gemm.o gstreamer.o gtk_show.o convert.o draw.o gemm_neon.o#中间过程所涉及的.o文件	  
+	  blas.o gemm.o gstreamer.o gtk_show.o convert.o draw.o gemm_neon.o UltraFace.o#中间过程所涉及的.o文件	  
 OBJDIR = ./obj/#存放.o文件的文件夹
 OBJS = $(addprefix $(OBJDIR), $(OBJ))#添加路径
 
@@ -39,13 +44,14 @@ all: obj $(TARGET)
 
 #将所有的.o文件链接成最终的可执行文件，需要库目录和库名，注意，OBJS要在LIBS之前。另外，如果要指定.o的生成路径，需要保证TARGET的依赖项是含路径的
 $(TARGET):$(OBJS)
-		$(CC) $(OBJS) $(CFLAGS) $(LDFLAGS) $(LIBS) -o $(TARGET)
+		$(CXX) $(OBJS) $(CFLAGS) $(LDFLAGS) $(LIBS) -o $(TARGET)
 #这个不是静态模式，而是通配符，第一个%类似bash中的*。
 $(OBJDIR)%.o: %.c
 		$(CC) -c $(CFLAGS) $< -o $@
 $(OBJDIR)%.o: %.s
 		$(CC) -c $(CFLAGS) $< -mfpu=neon -mcpu=cortex-a53 -o $@		
-
+$(OBJDIR)%.o: %.cpp
+		$(CXX) -c $(CFLAGS) $(CXXFLAGS) $< -o $@
 #用于生成存放.o文件的文件夹
 obj:
 		mkdir obj
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+extern "C"{
+#include "gstreamer.h"
+#include "gtk_show.h"
+#include "convert.h"
+#include "draw.h"
+#include "model.h"
+#include "parser.h"
+#include "network.h"
+#include "utils.h"
+#include "im2col.h"
+
+#include "my_common.h"
+#include "my_layer.h"
+// #include "event.h"
+}
+
+// #include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,19 +23,12 @@
 #include <unistd.h> //sleep
 #include <pthread.h>
 
-#include "parser.h"
-#include "network.h"
-#include "utils.h"
-#include "im2col.h"
-
-#include "my_common.h"
-#include "my_layer.h"
-#include "model.h"
-#include "gstreamer.h"
-#include "gtk_show.h"
-// #include "event.h"
-#include "convert.h"
-#include "draw.h"
+#include <iostream>
+#include <memory>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include "UltraFace.hpp"
 
 unsigned char *buffer_original;	 //暂存从相机抓取的原始图像帧
 unsigned char *buffer_processed; //从buffer_original copy过来的要转换后送入inference的图像帧
@@ -27,26 +37,9 @@ unsigned char *buffer_show;		 //从buffer_processed cpoy过来再加上目标框
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
-// event_parameter event1;
 
-// char coco_names[80][20] = {"person", "bicycle", "car", "motorbike", "aeroplane",
-// 						 "bus", "train", "truck", "boat", "traffic light",
-// 						 "fire hydrant", "stop sign", "parking meter", "bench", "bird",
-// 						 "cat", "dog", "horse", "sheep", "cow",
-// 						 "elephant", "bear", "zebra", "giraffe", "backpack",
-// 						 "umbrella", "handbag", "tie", "suitcase", "frisbee",
-// 						 "skis", "snowboard", "sports ball", "kite", "baseball bat",
-// 						 "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
-// 						 "wine glass", "cup", "fork", "knife", "spoon",
-// 						 "bowl", "banana", "apple", "sandwich", "orange",
-// 						 "broccoli", "carrot", "hot dog", "pizza", "donut",
-// 						 "cake", "chair", "sofa", "pottedplant", "bed",
-// 						 "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
-// 						 "remote", "keyboard", "cell phone", "microwave", "oven",
-// 						 "toaster", "sink", "refrigerator", "book", "clock",
-// 						 "vase", "scissors", "teddy bear", "hair drier", "toothbrush"};
 char coco_names[1][20] ={"face"};
-						 
+
 int main()
 {
 	gtk_show_init();
@@ -57,12 +50,12 @@ int main()
 	}
 	// image im = load_image_color("./resource/dog.jpg", 0, 0);
 	// image sized = letterbox_image(im, 416, 416);
-	buffer_input = malloc(WIDTH * HEIGHT * 3 * sizeof(float));
+	buffer_input = (float*)malloc(WIDTH * HEIGHT * 3 * sizeof(float));
 	image **alphabet = load_alphabet();	
-	network *net = yolov3_tiny(1, 1);
+	network *net = yolov3_tiny(1, 0);
 	// network *net = test_model();
 	// network *net = vgg16_adjusted(2);
-	load_weights(net, "./resource/yolov3-tiny_120000_q.weights");
+	load_weights(net, "./resource/yolov3-tiny_120000.weights");
 	sleep(1);
 	printf("go\r\n");
 	while (1)
@@ -73,7 +66,6 @@ int main()
 		pthread_mutex_unlock(&mutex1);
 
 		// rgb2bgr(buffer_processed, buffer_original, WIDTH, HEIGHT);
-
 		nhwc2nchw_char2float(buffer_input, buffer_processed, WIDTH, HEIGHT); //gst图像转换到darknet图像
 
 		double time_start = what_time_is_it_now();
@@ -97,3 +89,38 @@ int main()
 
 	return 0;
 }
+// int main()
+// {
+//     image **alphabet = load_alphabet();	
+//     gtk_show_init();
+//     gstreamer_init();
+//     UltraFace ultraface("./resource/RFB-320.mnn", WIDTH, HEIGHT, 4, 0.65); // config model input
+
+//     sleep(1);
+//     printf("go\r\n");
+//     while(1)
+//     {
+//         pthread_mutex_lock(&mutex1);// 上锁失败代表别的线程在使用，则当前线程阻塞
+// 		memcpy(buffer_processed, buffer_original, WIDTH*HEIGHT*3); // 给inference
+// 		pthread_mutex_unlock(&mutex1);
+
+//         // double time_start = what_time_is_it_now();
+//         std::vector<FaceInfo> face_info;
+//         ultraface.detect(buffer_processed,face_info);
+//         // double t = what_time_is_it_now() - time_start;
+// 		// printf("network_predict used: %f s\n", t);
+
+//         int nboxes = face_info.size();
+//         float thresh = .5;
+//         detection *dets = new detection[nboxes];
+//         ultraface.faceinfo2det(face_info, dets);
+//         my_draw_dets(buffer_processed, WIDTH, HEIGHT, dets, nboxes, thresh, alphabet, coco_names);
+//         free(dets);
+
+//         pthread_mutex_lock(&mutex2);							   // 上锁失败代表别的线程在使用，则当前线程阻塞
+// 		memcpy(buffer_show, buffer_processed, WIDTH * HEIGHT * 3); // 准备给gtk
+// 		pthread_mutex_unlock(&mutex2);
+//     }
+//     return 0;
+// }
+
